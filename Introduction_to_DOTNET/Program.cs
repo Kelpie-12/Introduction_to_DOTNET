d1ba2dfcb6a52e92f418bfede025f655cc13c820
Roman Yarkov <roman121998yarkov@gmail.com>
2024-06-16 20:04:30 +0400

    5)

--- Introduction_to_DOTNET/Program.cs
+++ Introduction_to_DOTNET/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,20 +24,132 @@ namespace Introduction_to_DOTNET
             //Console.SetCursorPosition(22, 11);
             Console.WriteLine("Привет .NET");
             Console.ResetColor(); 
-#endif
+			/*
 			Console.Write("Введите ваше имя: ");
 			string first_name = Console.ReadLine();
 			Console.Write("Введите вашу фамилию ");
 			string second_name = Console.ReadLine();
 			Console.Write("Введите ваш возраст ");
-			int age = Convert.ToInt32(Console.ReadLine());
-
-			/*Console.WriteLine(first_name);
+			int age = Convert.ToInt32(Console.ReadLine());Console.WriteLine(first_name);
             Console.WriteLine(second_name);
             Console.WriteLine(age);
             Console.WriteLine(first_name + " " + second_name + " " + age);
 			Console.WriteLine(string.Format("{0} {1} {2}", first_name, second_name, age));
 			Console.WriteLine($"{first_name} {second_name} {age}");*/
+#endif
+
+			int length = 5;
+			Console.WriteLine("0)");
+			for (int i = 0; i < length; i++)
+			{
+				for (int j = 0; j < length; j++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine("1)");
+			Console.WriteLine();
+			for (int i = 0; i < length; i++)
+			{
+				for (int j = 0; j < length; j++)
+				{
+					if (i >= j)
+					{
+						Console.Write("*");
+					}
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine("2)");
+			Console.WriteLine();
+			for (int i = 0; i < length; i++)
+			{
+				for (int j = 0; j < length; j++)
+				{
+					if (i <= j)
+					{
+						Console.Write("*");
+					}
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine("3)");
+			Console.WriteLine();
+			for (int i = 0; i < length; i++)
+			{
+				for (int j = 0; j < length; j++)
+				{
+					if (i <= j)
+					{
+						Console.Write("*");
+					}
+					else
+					{
+						Console.Write(" ");
+					}
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine("4)");
+			Console.WriteLine();
+			for (int i = 0; i < length; i++)
+			{
+				for (int j = 0; j < length; j++)
+				{
+					if (i + j >= length - 1)
+					{
+						Console.Write("*");
+					}
+					else
+					{
+						Console.Write(" ");
+					}
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine("5)");
+			Console.WriteLine();
+			length += 2;
+			for (int i = 0; i < length; i++)
+			{
+				for (int j = 0; j < length * 2; j++)
+				{
+					if (j == ((length + 1) / 2 + i + (length / 2)))
+					{
+						Console.Write("\\");
+					}
+					else if (j == length / 2 - i +3)
+					{
+						Console.Write("/");
+					}
+					else
+					{
+						Console.Write(" ");
+					}
+				}
+				Console.WriteLine();
+			}
+			for (int i = 0; i < length; i++)
+			{
+				for (int j = 0; j < length * 2; j++)
+				{
+					if (i == j)
+					{
+						Console.Write("\\");
+					}
+					else if (j == length - i + length - 1)
+					{
+						Console.Write("/");
+					}
+					else
+					{
+						Console.Write(" ");
+					}
+				}
+				Console.WriteLine();
+			}
+
 		}
 	}
 }
