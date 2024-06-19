c7ba3dedbe48de19cecdff16680e5b0eaa07251e
Roman Yarkov <roman121998yarkov@gmail.com>
2024-06-19 16:16:02 +0400

    calculator 1.0

---
+++ Calculator_v2.0/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_v2._0
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите простое арифметическое выражение -> ");
+			string expression=Console.ReadLine();
+			expression = expression.Replace('.', ',');
+			Console.WriteLine(expression);
+			String[] numbers=expression.Split('/','*','-','+');
+			//for (int i = 0; i < numbers.Length; i++)
+			//{
+			//	Console.Write(numbers[i]+"\t");
+			//}
+			double a = Convert.ToDouble(numbers[0]);
+			double b = Convert.ToDouble(numbers[1]);
+			char s = expression[expression.IndexOfAny(new char[]{'/', '*', '-', '+' })];
+			Console.WriteLine(s);
+			Console.WriteLine(a);
+			Console.WriteLine(b);
+
+			switch (s)
+			{
+				case '+':
+					Console.WriteLine($"{a}+{b} = {a+b}");
+					break;
+						case '*':
+					Console.WriteLine($"{a}*{b} = {a * b}");
+					break;
+				case '/':
+					Console.WriteLine($"{a}/{b} = {a / b}");
+					break;
+				case '-':
+					Console.WriteLine($"{a}-{b} = {a - b}");
+					break;
+				default:
+					break;
+			}
+			Main(args);
+		}
+	}
+}
