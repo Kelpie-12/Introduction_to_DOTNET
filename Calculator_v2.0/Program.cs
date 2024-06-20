f992b2fd1e1285b9177b7699fcc19b7a6c4a0b0b
Roman Yarkov <roman121998yarkov@gmail.com>
2024-06-20 12:25:55 +0400

    Recursion done

--- Calculator_v2.0/Program.cs
+++ Calculator_v2.0/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿//#define calc_1
+#define calc_2
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,18 +13,19 @@ namespace Calculator_v2._0
 	{
 		static void Main(string[] args)
 		{
+#if calc_1
 			Console.Write("Введите простое арифметическое выражение -> ");
-			string expression=Console.ReadLine();
+			string expression = Console.ReadLine();
 			expression = expression.Replace('.', ',');
 			Console.WriteLine(expression);
-			String[] numbers=expression.Split('/','*','-','+');
+			String[] numbers = expression.Split('/', '*', '-', '+');
 			//for (int i = 0; i < numbers.Length; i++)
 			//{
 			//	Console.Write(numbers[i]+"\t");
 			//}
 			double a = Convert.ToDouble(numbers[0]);
 			double b = Convert.ToDouble(numbers[1]);
-			char s = expression[expression.IndexOfAny(new char[]{'/', '*', '-', '+' })];
+			char s = expression[expression.IndexOfAny(new char[] { '/', '*', '-', '+' })];
 			Console.WriteLine(s);
 			Console.WriteLine(a);
 			Console.WriteLine(b);
@@ -29,9 +33,9 @@ namespace Calculator_v2._0
 			switch (s)
 			{
 				case '+':
-					Console.WriteLine($"{a}+{b} = {a+b}");
+					Console.WriteLine($"{a}+{b} = {a + b}");
 					break;
-						case '*':
+				case '*':
 					Console.WriteLine($"{a}*{b} = {a * b}");
 					break;
 				case '/':
@@ -43,7 +47,36 @@ namespace Calculator_v2._0
 				default:
 					break;
 			}
-			Main(args);
+			
+#endif
+			bool correct = true;
+			do
+			{
+				correct=true;
+				Console.Write("Введите простое арифметическое выражение -> ");
+				string expression = Console.ReadLine();
+				string s_operations = "+-/*";
+				String[] s_numbers = expression.Split(/*'/', '*', '-', '+'*/s_operations.ToCharArray());
+				s_numbers=s_numbers.Where(item=> item!="").ToArray();
+				double[] d_numbers = new double[s_numbers.Length];
+				for (int i = 0; i < s_numbers.Length; i++)
+				{
+					d_numbers[i] = Convert.ToDouble(s_numbers[i]);
+					Console.Write(d_numbers[i] + "\t");
+				}
+				Console.WriteLine();
+				char[] c_operations = expression.Where(item => s_operations.Contains(item)).ToArray();
+				for (int i = 0; i < c_operations.Length; i++)
+				{
+					Console.Write(c_operations[i] + "\t");
+				}
+				Console.WriteLine();
+				if (d_numbers.Length!=c_operations.Length+1)
+				{
+					correct = false;
+				}
+			} while (!correct);
+			//Main(args);
 		}
 	}
 }
---
+++ Recursion_2.0/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_2._0
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"Factorial 4 = {Factorial(4)}");
+			Console.WriteLine($"Power 2.5^3 = {Power(2.5, 3)}");
+			Console.WriteLine($"fibonachi 10 = {fibonachi(10)}");
+			Console.WriteLine("Заданное количество чисел из ряда Фибоначчи (10)");
+			print_fibonachi(10);
+			Console.WriteLine("Фибоначчи, до указанного предела (33)");
+			print_fibonachi_predel(33);
+		}
+		static public int Factorial(int number)
+		{
+			if (number == 1)
+				return 1;
+			return Factorial(number - 1) * number;
+		}
+		static public double Power(double number, int power)
+		{
+			if (power == 0)
+				return 1;
+			return number * Power(number, --power);
+		}
+		static public int fibonachi(int number)
+		{
+			if (number == 1 || number == 2)
+				return 1;
+			if (number == 0)
+				return 0;
+			return fibonachi(number - 1) + fibonachi(number - 2);
+		}
+		static public void print_fibonachi(int iter)
+		{
+			for (int i = 1; i < iter + 1; i++)
+			{
+				Console.WriteLine($"fibonachi {i} = {fibonachi(i)}");
+			}
+		}
+		static public void print_fibonachi_predel(int iter)
+		{
+			int i = 0;
+			while (iter >= fibonachi(i))
+			{
+				Console.WriteLine($"fibonachi {i} = {fibonachi(i)}");
+				i++;
+			}
+		}
+	}
+}
